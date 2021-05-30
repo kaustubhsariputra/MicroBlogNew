6b1c828d02704d7397f566b025b1a5c7eccfe581
Kaustubh <kaustubhsariputra08@gmail.com>
2021-05-30 13:30:14 +0530

    Psycopg2-binary and PostgreSQL

--- MicroBlog/__init__.py
+++ MicroBlog/__init__.py
@@ -9,7 +9,8 @@ from flask_mail import Mail
 
 app = Flask(__name__)
 app.config['SECRET_KEY']= os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
