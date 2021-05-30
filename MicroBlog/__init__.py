6f48c79bc0e2b83639d38011b96ff8587b630930
Kaustubh <kaustubhsariputra08@gmail.com>
2021-05-30 13:52:46 +0530

    Psycopg2-binary and PostgreSQL

--- MicroBlog/__init__.py
+++ MicroBlog/__init__.py
@@ -5,11 +5,15 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import requests
 from flask_mail import Mail
+import re
 
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
 
 app = Flask(__name__)
 app.config['SECRET_KEY']= os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
